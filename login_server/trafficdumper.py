59751c9bc98fbe5b2e5f1432d04abed825a6b4fe
Maurice van der Pot <griffon26@kfk4ever.com>
2022-02-07 23:20:33 +0100

    Update copyright notices with new project name

--- login_server/trafficdumper.py
+++ login_server/trafficdumper.py
@@ -2,20 +2,20 @@
 #
 # Copyright (C) 2018  Maurice van der Pot <griffon26@kfk4ever.com>
 #
-# This file is part of taserver
+# This file is part of gaserver
 # 
-# taserver is free software: you can redistribute it and/or modify
+# gaserver is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 # 
-# taserver is distributed in the hope that it will be useful,
+# gaserver is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 # 
 # You should have received a copy of the GNU Affero General Public License
-# along with taserver.  If not, see <http://www.gnu.org/licenses/>.
+# along with gaserver.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 dumpfilename = 'gaserverdump.carrays'
