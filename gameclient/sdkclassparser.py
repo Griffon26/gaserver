df2ffa5e409f7f2f5fee3979962cd4ba8f98b3ef
ihateAdmins <prozockermt2@hotmail.de>
2022-03-03 14:59:37 +0100

    sdkclassparser json added

--- gameclient/sdkclassparser.py
+++ gameclient/sdkclassparser.py
@@ -1,17 +1,43 @@
 import json
 from pathlib import Path
-
+import re
 
 def main():
     classes = {}
+    sdkpath = Path('I:/games/Global Agenda/gamods/Global Agenda/SDK_HEADERS')
+    inclass = True
+    firstline = True
 
-    sdkpath = Path('C:/git/gamods/Global Agenda/SDK_HEADERS')
-    for file in sdkpath.glob('*_classes.h'):
-        with open(file) as f:
-            for line in f:
-                # do something
-                pass
-
+    with open('parsed_classes.tmp', 'wt') as outfile:
+        outfile.write("{")
+        for file in sdkpath.glob('*_classes.h'):
+            with open(file, 'rt') as infile:
+                    for line in infile:
+                        if re.search(r'^class\ (.+)\ \:\ .*', line):
+                            res_str = re.sub(r"^class\ (.+)\ \:\ .*", r"'\1': {",line)
+                            if firstline:
+                                inclass = True
+                                firstline = False
+                            else:
+                                inclass = False
+                                outfile.write("},")
+                            outfile.write(res_str.replace("'","\""))
+                        elif "CPF_Net" in line:
+                            if re.search(r'^\t([a-zA-Z]*)\ *([a-zA-Z]+)\ +([a-zA-Z]+)\;.+$', line):
+                                #res_str = re.sub(r"^\t([a-zA-Z]*)\ *([a-zA-Z]+)\ +([a-zA-Z]+)\;.+$", r"'\3': '\2',", line)
+                                res_str = re.sub(r"^\t([a-zA-Z]+)\ *([a-zA-Z]*)\ +([a-zA-Z]+)\;.+$", r"'\3': '\1 \2',", line).replace("struct ", "").replace(" ',","',")
+                                outfile.write(res_str.replace("'","\""))
+        #"s/,\n}//g"
+        outfile.write("}")
+        outfile.write("}")
+        outfile.close()
+        with open('parsed_classes.tmp', 'r') as inf:
+            filestr = inf.read()
+            content_new = re.sub(',\n}', r'\n}', filestr)
+            var = json.loads(content_new)
+            print(var)
+            open('parsed_classes.json', 'wt').write(content_new)
+main()
 
 '''
 For code like this:
